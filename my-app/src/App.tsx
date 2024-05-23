b6c919a17e721adf0a4989d6a8d39cf87aa22139
Gosia Szulc <malgorzata@leanconvert.com>
2024-05-23 21:41:06 +0200

    added union type

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
   const details = {
@@ -17,6 +18,7 @@ function App() {
       <Greet name='Gosia' count={11} isLoggedIn={false}/>
       <Person details={details} />
       <PersonList names={names} />
+      <Status status={'loading'}/>
     </div>
   );
 }
